e7cc76d0054115c0c114de1db480d36d9a7a01ab
Yuichi Tanaka <yuichielectric@github.com>
2021-05-12 16:33:42 +0900

    Comment test cases

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,8 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-
+/*
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
@@ -26,3 +26,4 @@ test('test runs', () => {
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
+*/
